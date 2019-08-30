0da516636654e9a514554326317a85fe89869ea7
Peter Williams <peter.williams@papercut.com>
2019-08-30 15:31:18 +1000

    About to segment into multiple foreground images

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/peterwilliams97/ocropy
 go 1.12
 
 require github.com/unidoc/unipdf/v3 v3.1.1
+
+replace github.com/unidoc/unipdf/v3 => /Users/peter/go-work/src/github.com/unidoc/unipdf
--- segment.go
+++ segment.go
@@ -27,6 +27,7 @@ import (
 	"os"
 	"path/filepath"
 
+	"github.com/unidoc/unipdf/v3/common"
 	"github.com/unidoc/unipdf/v3/core"
 	"github.com/unidoc/unipdf/v3/creator"
 )
@@ -40,6 +41,8 @@ func init() {
 const usage = "Make masked image"
 
 func main() {
+	common.SetLogger(common.NewConsoleLogger(common.LogLevelInfo))
+
 	var inPath, maskPath, outPath string
 	flag.StringVar(&inPath, "i", "", "Input image.")
 	flag.StringVar(&maskPath, "m", "", "JSON file containing rectangles of images.")
@@ -74,6 +77,9 @@ func main() {
 		panic(err)
 	}
 
+	bounds := img.Bounds()
+	w, h := bounds.Max.X, bounds.Max.Y
+
 	dilation := 10
 	fgd := makeForeground(img, dilate(rects, dilation))
 	bgd := makeBackground(img, dilate(rects, -dilation))
@@ -102,33 +108,56 @@ func main() {
 	}
 	fmt.Printf("saved background to %q\n", bgdPath)
 
-	overlay(bgdPath, fgdPath, outPath)
+	overlayImages(bgdPath, fgdPath, outPath, w, h)
 }
 
 const (
-	width  = 210.0 / 25.4 * 72.0 / 2
-	height = 297.0 / 25.4 * 72.0 / 2
-	xPos   = width / 5.0
-	yPos   = height / 5.0
+	widthMM    = 210.0
+	heightMM   = 297.0
+	widthInch  = 8.5
+	heightInch = 11.0
+	// widthPt  = (widthMM / 25.4) * 72.0
+	// heightPt = (heightMM / 25.4) * 72.0
+	widthPt  = widthInch * 72.0
+	heightPt = heightInch * 72.0
+	// xPos     = widthPt / 10.0
+	// yPos     = heightPt / 10.0
+	xPos   = 0.0
+	yPos   = 0.0
+	width  = widthPt - 2*xPos
+	height = heightPt - 2*yPos
 )
 
-// Add image to a specific page of a PDF.  xPos and yPos define the upper left corner of the image location, and iwidth
+// computeScale returns the scale for w x h -> width x height
+func computeScale(width, height, w, h float64) float64 {
+	xScale := width / w
+	yScale := height / h
+	if xScale < yScale {
+		return xScale
+	}
+	return yScale
+}
+
+// overlay image in `fgdPath` over image in `bgdPath` (currently assumed to be have the same
+// dimensions `w` x `h`) and write the resulting single page `width` x `height` PDF to `outPath`.
 // is the width of the image in PDF document dimensions (height/width ratio is maintained).
-func overlay(bgdPath, fgdPath, outPath string) error {
+func overlayImages(bgdPath, fgdPath, outPath string, w, h int) error {
+	scale := computeScale(width, height, float64(w), float64(h))
 	c := creator.New()
 	c.NewPage()
 
-	if err := addImage(c, bgdPath, core.NewDCTEncoder()); err != nil {
+	if err := addImage(c, bgdPath, core.NewFlateEncoder(), scale); err != nil { // !@#$ DCT
 		return err
 	}
-	if err := addImage(c, fgdPath, core.NewFlateEncoder()); err != nil {
+	if err := addImage(c, fgdPath, core.NewFlateEncoder(), scale); err != nil {
 		return err
 	}
 
 	return c.WriteToFile(outPath)
 }
 
-func addImage(c *creator.Creator, imgPath string, encoder core.StreamEncoder) error {
+// addImage adds image in `imagePath` to `c` with encoding and scale given by `encoder` and `scale`.
+func addImage(c *creator.Creator, imgPath string, encoder core.StreamEncoder, scale float64) error {
 	img, err := c.NewImageFromFile(imgPath)
 	if err != nil {
 		return err
@@ -136,11 +165,15 @@ func addImage(c *creator.Creator, imgPath string, encoder core.StreamEncoder) er
 	if encoder != nil {
 		img.SetEncoder(encoder)
 	}
-	img.ScaleToWidth(width)
+	common.Log.Info("addImage: widthMM=%.2f heightMM=%.2f", widthMM, heightMM)
+	common.Log.Info("addImage: widthPt=%.2f heightPt=%.2f", widthPt, heightPt)
+	common.Log.Info("addImage: xPos=%.2f yPos=%.2f width=%.2f height=%.2f", xPos, yPos, width, height)
+	img.Scale(scale, scale)
 	img.SetPos(xPos, yPos)
 	return c.Draw(img)
 }
 
+// makeForeground returns `img` masked to the rectangles in `rectList`.
 func makeForeground(img image.Image, rectList []Rect) image.Image {
 	bounds := img.Bounds()
 	w, h := bounds.Max.X, bounds.Max.Y
@@ -159,6 +192,7 @@ func makeForeground(img image.Image, rectList []Rect) image.Image {
 	return rgba
 }
 
+// makeForegroundList images of returns `img` clipped by the rectangles in `rectList`.
 func makeForegroundList(img image.Image, rectList []Rect) []image.Image {
 	bounds := img.Bounds()
 	w, h := bounds.Max.X, bounds.Max.Y
@@ -169,16 +203,17 @@ func makeForegroundList(img image.Image, rectList []Rect) []image.Image {
 	fmt.Printf("w=%d h=%d\n", w, h)
 
 	// fgdList := make([]*image.RGBA, len(rectList))
-	fgdList := make([]*image.Image, len(rectList))
+	fgdList := make([]image.Image, len(rectList))
 	for i, r := range rectList {
-		rgba := image.NewRGBA(r.bounds())
-		draw.Draw(rgba, r.bounds(), img, image.ZP, draw.Src)
+		rgba := image.NewRGBA(r.zpBounds())
+		draw.Draw(rgba, r.zpBounds(), img, image.ZP, draw.Src)
 		fgdList[i] = rgba
 		fmt.Printf("%4d: %v -> %v\n", i, r, rgba.Bounds())
 	}
 	return fgdList
 }
 
+// makeForeground returns `img` not masked to the rectangles in `rectList`.
 func makeBackground(img image.Image, rectList []Rect) image.Image {
 	bounds := img.Bounds()
 	w, h := bounds.Max.X, bounds.Max.Y
@@ -195,11 +230,6 @@ func makeBackground(img image.Image, rectList []Rect) image.Image {
 	}
 	return rgba
 }
-
-// var rectList = []Rect{
-// 	Rect{50, 50, 450, 650},
-// 	Rect{500, 1000, 900, 1800},
-// }
 
 type Rect struct {
 	X0, Y0, X1, Y1 int
@@ -222,6 +252,11 @@ func (r Rect) position() image.Point {
 	return image.Point{r.X0, r.Y0}
 }
 
+func (r Rect) zpBounds() image.Rectangle {
+	return image.Rect(0, 0, r.X1-r.X0, r.Y1-r.Y0)
+}
+
+// dilate returns the Rects in `rectList` dilated by `d` on all 4 sides
 func dilate(rectList []Rect, d int) []Rect {
 	outList := make([]Rect, len(rectList))
 	for i, r := range rectList {
